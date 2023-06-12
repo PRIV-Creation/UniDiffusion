b30f089e8509e091f535793d8ea9c50d870098e9
sariel <2788787973@qq.com>
2023-06-12 22:53:56 +0800

    update setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="diffusion_trainer",
+    version="0.1",
+    packages=find_packages(),
+    author="PRIV-Creation",
+    author_email="priv@bupt.edu.cn",
+    description="Diffusion training toolbox.",
+    license="Apache",
+    keywords="diffusion",
+    url="https://github.com/PRIV-Creation/DiffusionTrainer",   # project home page, if any
+)

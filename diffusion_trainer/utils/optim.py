36275c6624a33f1c5646cb536d8012697b7b431e
sariel <2788787973@qq.com>
2023-06-13 10:39:34 +0800

    support 8bit adamw

--- diffusion_trainer/utils/optim.py
+++ diffusion_trainer/utils/optim.py
@@ -1,13 +1,14 @@
 import torch
-import torch
-import copy
-from typing import Any, Dict, List, Set, Optional, Callable
 
 
 def get_optimizer(optimizer='SGD', **kwargs):
+    if optimizer == '8bit_adam':
+        import bitsandbytes as bnb
+
     optimizer_map = {
         'sgd': torch.optim.SGD,
         'adam': torch.optim.Adam,
         'adamw': torch.optim.AdamW,
+        '8bit_adam': bnb.optim.AdamW8bit
     }
     return optimizer_map[optimizer.lower()](**kwargs)

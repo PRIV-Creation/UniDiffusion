8587612c6cc349c55a5a13b52dba017944cde2e7
sariel <2788787973@qq.com>
2023-06-21 23:09:34 +0800

    update

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="unidiffusion",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     author="PRIV-Creation",
     author_email="priv@bupt.edu.cn",
     description="Diffusion training toolbox.",
-    license="Apache",
-    keywords="diffusion",
+    license="Apache-2.0 license",
+    keywords="diffusion, stable-diffusion, dreambooth, lora",
     url="https://github.com/PRIV-Creation/UniDiffusion",   # project home page, if any
 )

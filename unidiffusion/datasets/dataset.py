8dd6e7dedea114aa36cff5c26c9d39a22a5c1182
sariel <2788787973@qq.com>
2023-11-28 14:29:03 +0800

    feat(dataset): init concat dataset

--- unidiffusion/datasets/dataset.py
+++ unidiffusion/datasets/dataset.py
@@ -15,3 +15,24 @@ class BaseDataset(Dataset):
         pass
 
 
+class ConcatDataset(Dataset):
+    def __init__(self, datasets):
+        self.datasets = datasets
+        self._length = sum([len(dataset) for dataset in datasets])
+
+        # construct index
+        self.dataset_index = []
+        self.data_index = []
+        for d in datasets:
+            for i in range(len(d)):
+                self.dataset_index.append(d)
+                self.data_index.append(i)
+
+    def __len__(self):
+        return self._length
+
+    def __getitem__(self, index):
+        return self.datasets[self.dataset_index[index]][self.data_index[index]]
+
+    def get_prompt(self, item):
+        return self.datasets[self.dataset_index[item]].get_prompt(self.data_index[item])
